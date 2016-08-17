7e4b934019e53de3bcb31972172b3b0cba687a91
tafelito <tafelito@gmail.com>
2016-08-17 15:39:09 -0400

    Added simple plugin to pop up they keyboard when focusing an input field.

---
+++ src/ios/CDVKeyboardDisplay.m
@@ -0,0 +1,31 @@
+//
+//  CDVRestrictKeyboards.m
+//  Oppa
+//
+//  Extends the AppDelegate to pop up the keyboard when programmatically focussing an input field.
+
+#import <objc/runtime.h>
+#import "AppDelegate.h"
+
+@implementation AppDelegate(AppDelegate)
+
+/**
+ * This is main kick off after the app inits, the views and Settings are setup here. (preferred - iOS4 and up)
+ */
+- (BOOL)application:(UIApplication*)application didFinishLaunchingWithOptions:(NSDictionary*)launchOptions
+{
+  [self keyboardDisplayDoesNotRequireUserAction];
+}
+
+- (void) keyboardDisplayDoesNotRequireUserAction {
+  SEL sel = sel_getUid("_startAssistingNode:userIsInteracting:blurPreviousNode:userObject:");
+  Class WKContentView = NSClassFromString(@"WKContentView");
+  Method method = class_getInstanceMethod(WKContentView, sel);
+  IMP originalImp = method_getImplementation(method);
+  IMP imp = imp_implementationWithBlock(^void(id me, void* arg0, BOOL arg1, BOOL arg2, id arg3) {
+    ((void (*)(id, SEL, void*, BOOL, BOOL, id))originalImp)(me, sel, arg0, TRUE, arg2, arg3);
+  });
+  method_setImplementation(method, imp);
+}
+
+@end
